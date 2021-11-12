eb94f3f500ba9eb772819f390a69829a21229489
Kalcho815 <kaloynwa@gmail.com>
2021-11-12 13:31:56 +0000

    fix

---
+++ Tests.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Tests {
+
+    @Test
+    public void testStringInput(){
+        String input = "Ok";
+        Assert.assertEquals("Argument is not an integer", Dec2Hex.convert(new String[]{input}));
+    }
+
+    @Test
+    public void testEmptyInput(){
+        Assert.assertEquals("No arguments provided.", Dec2Hex.convert(new String[]{}));
+    }
+}
