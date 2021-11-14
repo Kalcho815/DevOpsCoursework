303e791c73ca2701ab0b7702d3b69f4635114db0
Kalcho815 <kaloynwa@gmail.com>
2021-11-14 21:30:28 +0000

    added comments

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -2,13 +2,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Tests {
-
+	
+    //testing the case where the input is not an integer
     @Test
     public void testStringInput(){
         String input = "Ok";
         Assert.assertEquals("Argument is not an integer", Dec2Hex.convert(new String[]{input}));
     }
-
+    
+    //testing the case where there are not arguments provided
     @Test
     public void testEmptyInput(){
         Assert.assertEquals("No arguments provided.", Dec2Hex.convert(new String[]{}));
